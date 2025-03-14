2d830298f40a67b169e49a68a54fff33785fc554
VenkateshLatchireddy <venkyv676@gmail.com>
2025-03-14 17:56:30 +0530

    railway updated

--- config/database.js
+++ config/database.js
@@ -3,8 +3,22 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT,
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // ✅ Ignore self-signed SSL error
+    },
+    connectTimeout: 60000 // ✅ Increase timeout
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 60000,
+    idle: 10000
+  }
 });
 
 sequelize.authenticate()
